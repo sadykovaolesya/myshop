e80301e9205c0a73de65660e0b94afe1f294527a
Olesya <progafox@gmail.com>
2021-01-18 14:40:51 +0500

    Creat models Product and category

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Product, ProdactCategiry
+
+admin.site.register(ProdactCategiry)
+admin.site.register(Product)
+
 # Register your models here.
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class ProdactCategiry(models.Model):
+    name = models.CharField(verbose_name="name", max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(ProdactCategiry, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="name_product", max_length=128)
+    image = models.ImageField(upload_to = "product_images", blank=True)
+    description = models.TextField(verbose_name="description", blank=True)
+    price = models.DecimalField(verbose_name="price", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="quantity in store", default=0)
+
+    def __str__(self):
+        return f"{self.name}({self.category})"
 # Create your models here.
